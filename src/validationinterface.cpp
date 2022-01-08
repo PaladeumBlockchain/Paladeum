966fc88002f5b4b490b92002c2e044568745c625
volbil <volbilnexus@gmail.com>
2022-01-09 00:06:26 +0200

    Fix shutdown in case of errors during initialization

--- src/validationinterface.cpp
+++ src/validationinterface.cpp
@@ -54,7 +54,9 @@ void CMainSignals::UnregisterBackgroundSignalScheduler() {
 }
 
 void CMainSignals::FlushBackgroundCallbacks() {
-    m_internals->m_schedulerClient.EmptyQueue();
+    if (m_internals) {
+        m_internals->m_schedulerClient.EmptyQueue();
+    }
 }
 
 CMainSignals& GetMainSignals()
@@ -89,6 +91,10 @@ void UnregisterValidationInterface(CValidationInterface* pwalletIn) {
 }
 
 void UnregisterAllValidationInterfaces() {
+    if (!g_signals.m_internals) {
+        return;
+    }
+
     g_signals.m_internals->BlockChecked.disconnect_all_slots();
     g_signals.m_internals->Broadcast.disconnect_all_slots();
     g_signals.m_internals->SetBestChain.disconnect_all_slots();
