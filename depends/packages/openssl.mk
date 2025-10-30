b72b9af431e0b5457d9bec1cb8d9712a453a8803
volbil <volbilnexus@gmail.com>
2025-10-30 11:19:49 +0200

    Depends update

--- depends/packages/openssl.mk
+++ depends/packages/openssl.mk
@@ -1,9 +1,8 @@
 package=openssl
-$(package)_version=1.1.1k
+$(package)_version=1.1.1w
 $(package)_download_path=https://www.openssl.org/source
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=892a0875b9872acd04a9fde79b1f943075d5ea162415de3047c327df33fbaee5
-
+$(package)_sha256_hash=cf3098950cb4d853ad95c0841f1f9c6d3dc102dccfcacd521d93925208b76ac8
 define $(package)_set_vars
 $(package)_config_opts=--prefix=$(host_prefix) --openssldir=$(host_prefix)/etc/openssl
 $(package)_config_opts+=no-camellia
@@ -34,6 +33,10 @@ $(package)_config_opts+=no-weak-ssl-ciphers
 $(package)_config_opts+=no-whirlpool
 $(package)_config_opts+=no-zlib
 $(package)_config_opts+=no-zlib-dynamic
+$(package)_config_opts+=no-chacha
+$(package)_config_opts+=no-poly1305
+$(package)_config_opts+=no-asm
+# $(package)_config_opts+=no-sha1
 $(package)_config_opts+=$($(package)_cflags) $($(package)_cppflags)
 $(package)_config_opts_linux=-fPIC -Wa,--noexecstack
 $(package)_config_opts_x86_64_linux=linux-x86_64
@@ -45,6 +48,7 @@ $(package)_config_opts_mips_linux=linux-generic32
 $(package)_config_opts_powerpc_linux=linux-generic32
 $(package)_config_opts_x86_64_darwin=darwin64-x86_64-cc
 $(package)_config_opts_arm_darwin=darwin64-arm64-cc
+$(package)_config_opts_aarch64_darwin=darwin64-arm64-cc
 
 $(package)_config_opts_x86_64_mingw32=mingw64
 $(package)_config_opts_i686_mingw32=mingw32
